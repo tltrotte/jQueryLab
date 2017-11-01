e6776fe586a318a8c769cb2d1a335f2f741fffee
Tamara Trotter <tltrotte@me.com>
2017-10-31 21:55:05 -0400

    adding jquery functions

--- lab.js
+++ lab.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
 console.log("Its Working!!!")
+
+$("i").on("click", function() {
+  		$("#popUp").css("display", "none");
+  	});
+
+$(".available").on("click", function(){
+  $("#popUp").css("display", "flex");
+});
+
+  $("#reserve").on("click", function(){
+    $(this).toggleClass("reserved");
+    console.log("reserved");
+  });
+
+  /*  $(".table").on("click", function() {
+  var tablenum = $(this).attr("data-tablenum");
+  tablenum = parseInt(tablenum);
+  lastTableClicked = this;
+});*/
+
+
 });
